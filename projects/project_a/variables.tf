bd01d3166177bedf58bcbd107872405b60e769a8
ToruMakabe <tomakabe@microsoft.com>
2018-01-07 21:50:09 +0900

    remove location variables from variable files

--- projects/project_a/variables.tf
+++ projects/project_a/variables.tf
@@ -2,15 +2,6 @@ variable "resource_group_name" {
   default = "tf-sample"
 }
 
-variable "location" {
-  default = {
-    default.location = "japaneast"
-    dev.location     = "japaneast"
-    stage.location   = "japaneast"
-    prod.location    = "japaneast"
-  }
-}
-
 variable "cost-center" {
   default = "12345"
 }
